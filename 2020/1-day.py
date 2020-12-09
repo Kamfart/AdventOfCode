a62c38f205ca27a45017bf05785cd503d5470047
Lab0 <apiakamfart@protonmail.com>
2020-12-09 03:14:35 +0100

    day 1 completed

--- 2020/1-day.py
+++ 2020/1-day.py
@@ -1,5 +1,7 @@
-# Instruction : Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+# Instruction part 1 : Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
 # Exemple :     199 + 1821 = 2020 ; 199 * 1821 = key 
+
+# Instruction part 2 : Same, with 3 numbers.
 import sys, timeit
 
 tic=timeit.default_timer()
@@ -10,20 +12,6 @@ def chomp(s):
     return s[:-1] if s.endswith('\n') else s
 
 def solve1(listValue, valueWanted):
-    indexI = 0
-    for i in listValue:
-        indexJ = 0
-        for j in listValue:
-            if indexI == indexJ:
-                indexJ+=1
-            elif (int(i) + int(j)) == valueWanted:
-                return (int(i) * int(j))
-                
-            else:
-                indexJ+=1  
-        indexI+=1
-
-def solve2(listValue, valueWanted):
     listValue = [int(elm) for elm in listValue]
     indexI = 0
     for i in listValue:
@@ -38,6 +26,24 @@ def solve2(listValue, valueWanted):
                 indexJ+=1
         indexI+=1
 
+def solve2(listValue, valueWanted):
+    listValue = [int(elm) for elm in listValue]
+    indexI = 0
+    for i in listValue:
+        indexJ = 0
+        for j in listValue:
+            IndexK = 0
+            for k in listValue:
+                if indexI == indexJ == IndexK:
+                    indexJ+=1
+                elif (i + j + k) == valueWanted:
+                    return i * j * k
+                else:
+                    indexJ+=1
+        indexI+=1
+
+
+
 lClean = []
 with open(f,'r') as it:
     lines = it.readlines()
@@ -45,6 +51,7 @@ with open(f,'r') as it:
         lClean.append(chomp(l))
     del lines
 
+print(solve1(lClean, 2020))
 print(solve2(lClean, 2020))
 
 toc=timeit.default_timer()
