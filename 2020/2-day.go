9830a7017fcb234e51630dd976428a23beeb7953
Lab0 <apiakamfart@protonmail.com>
2020-12-10 02:28:26 +0100

    Main : parse file into table

--- 2020/2-day.go
+++ 2020/2-day.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	data, err := ioutil.ReadFile("/home/lab0-dev/Projets/AdventOfCode/2020/2-puzzle")
+	file, err := os.Open("/home/lab0-dev/Projets/AdventOfCode/2020/2-puzzle")
 	if err != nil {
-		fmt.Println("File reading error", err)
-		return
+		log.Fatal(err)
 	}
-	fmt.Println("Contents of file:", string(data))
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// [password, policyCar, policyMin, policyMax]
+		var policySlice = make([]string, 4)
+		policySlice[0] = strings.Split(line, ":")[1]
+		policy := strings.Split(line, ":")[0]
+		policySlice[1] = strings.Split(policy, " ")[1]
+		policyDigit := strings.Split(policy, " ")[0]
+		policySlice[2] = strings.Split(policyDigit, "-")[0]
+		policySlice[3] = strings.Split(policyDigit, "-")[1]
+
+		fmt.Println(policy)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
