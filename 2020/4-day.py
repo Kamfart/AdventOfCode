fcfd994e346e44c9aaff92409ad9663bd0402984
Lab0 <apiakamfart@protonmail.com>
2020-12-13 14:37:32 +0100

    Day 4 solved

--- 2020/4-day.py
+++ 2020/4-day.py
@@ -1,25 +1,23 @@
 import sys, timeit
 
+tic=timeit.default_timer()
+
 # Remove \n from string
 def chomp(s):
     return s[:-1] if s.endswith('\n') else s
 
 # Parse file and return a list
-# Algo :
-# Create 3 buffers :
-#   - listPassClean, contains passport data non normalized
-#   - listBuff, temporary list work
-#   - listFinal, contains passport data normalized as one field by slot list
-# Exemple In : 
+# Exemple :
+# IN : 
 # eyr:2035
 # byr:1988 hgt:193cm
 # iyr:2028 cid:128 hcl:#18171d ecl:utc pid:9743739773
-# Out : ['eyr:2035', 'byr:1988', 'hgt:193cm', 'iyr:2028', 'cid:128', 'hcl:#18171d', 'ecl:utc', 'pid:9743739773']
+# OUT : ['eyr:2035', 'byr:1988', 'hgt:193cm', 'iyr:2028', 'cid:128', 'hcl:#18171d', 'ecl:utc', 'pid:9743739773']
 def parseFile(listPassport):
     listPassClean = []
     listBuff = []
     
-    # add passport information on multiple line into dedicated list
+    # add passport information on multiple line into a buffer list
     # then add this new list into listPassClean (data non normalized)
     # Exemple :
     # IN :
@@ -34,9 +32,9 @@ def parseFile(listPassport):
         else:
             listBuff.append(line)
     
-    # NOTE : Because the file is not ending py "\n"
+    # NOTE : Because the file is not ending by "\n"
     # we must add the content of the buffer at the end of the loop
-    # Otherwise last line of file will not be handle.
+    # Otherwise the last line of the file will not be handle.
     listPassClean.append(listBuff)
 
     listBuff = []
@@ -88,6 +86,103 @@ def checkField(list2DPassport, listFieldRequired, listFieldOptional):
 
     return listClean
 
+# check if a field has the required length and if it is between a given interval
+def controlYear(field, lenDigit, start, end):
+    if len(field) != lenDigit or int(field) < start or int(field) > end:
+        return False
+    else:
+        return True
+
+# check if a field has the required unit and if it is between a given interval
+# param : min/max for cm and inches
+def controlHeight(field, minCm, maxCm, minIn, maxIn):
+    value=field[:-2]
+    unit=field[-2:]
+    if unit != "in" and unit != "cm":
+        return False
+    elif unit == "in" and (int(value) < minIn or int(value) > maxIn):
+        return False
+    elif unit == "cm" and (int(value) < minCm or int(value) > maxCm):
+        return False
+    else:
+        return True
+
+# check if a field has the required character and if it composed by hexadecimal value only
+def controlHairColor(field):
+    valueHexa=["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
+    if field[0] != "#":
+        return False
+    else:
+        for c in field[1:]:
+            if c not in valueHexa:
+                return False
+        return True
+
+# check if a field is include in a defined list
+def controlEyeColor(field):
+    color = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+    if field not in color:
+        return False
+    else:
+        return True
+
+# check if a field has the required length and if it's a digit
+def controlPassportID(field, lenID):
+    if len(field) != lenID or not field.isdigit():
+        return False
+    else:
+        return True
+
+# Main controle : 
+# Algo :
+# Iterate over 2DList, each _list of the 1st iteration is iterate in another loop
+# Each field of the 2nd itaration is parse in key and value variable
+# Each key has if own test, if one test is false, break the current loop
+# otherwise add the _list of the 1st iteration into a buffer list
+# Return the controled list
+def controlField(list2DPassport):
+    listClean = []
+
+    for _list in list2DPassport:
+        isOk = True
+
+        for kv in _list:
+            key = kv.split(":")[0]
+            value = kv.split(":")[1]
+            if key == "byr":
+                if not controlYear(value, 4, 1920, 2002):
+                    isOk = False
+                    break
+            elif key == "iyr":
+                if not controlYear(value, 4, 2010, 2020):
+                    isOk = False
+                    break
+            elif key == "eyr":
+                if not controlYear(value, 4, 2020, 2030):
+                    isOk = False
+                    break
+            elif key == "hgt":
+                if not controlHeight(value, 150, 193, 59, 76):
+                    isOk = False
+                    break
+            elif key == "hcl":
+                if not controlHairColor(value):
+                    isOk = False
+                    break
+            elif key == "ecl":
+                if not controlEyeColor(value):
+                    isOk = False
+                    break
+            elif key == "pid":
+                if not controlPassportID(value, 9):
+                    isOk = False
+                    break
+        if isOk :
+            listClean.append(_list)
+            
+
+    return listClean
+
 f=sys.argv[1]
 listFieldRequired = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 listFieldOptional = ["cid"]
@@ -101,5 +196,9 @@ with open(f,'r') as it:
 
 lClean = parseFile(lClean)
 lClean = checkField(lClean, listFieldRequired, listFieldOptional)
+lClean = controlField(lClean)
 
 print(len(lClean))
+
+toc=timeit.default_timer()
+print(toc - tic)
